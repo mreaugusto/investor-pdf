3e16c8c2a40ac8f5525acd6a3a41f3854a16bb3d
Eduardo Silva <eduardo.silva@sequenza.com.br>
2019-02-18 23:46:15 -0300

    Operations

--- src/main/java/com/eaugusto/investorpdf/Operations.java
+++ src/main/java/com/eaugusto/investorpdf/Operations.java
@@ -4,22 +4,17 @@ import java.io.File;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.eaugusto.investorpdf.api.BrokerReport;
 import com.eaugusto.investorpdf.api.InvestmentType;
-import com.eaugusto.investorpdf.api.Order;
 import com.eaugusto.investorpdf.facade.ClearFacade;
 import com.eaugusto.investorpdf.utils.PDFUtils;
 
-public class Main {
+public class Operations {
 
 	public static void main(String[] args) {
 
@@ -30,10 +25,7 @@ public class Main {
 			throw new RuntimeException(e);
 		}
 
-		File[] files = folder.listFiles(p -> p.getName().endsWith(".pdf"));
-
-		Map<String, Integer> assetsAmount = new HashMap<>();	
-		Map<String, List<Order>> assetsOrders = new HashMap<>();
+		File[] files = folder.listFiles(f -> f.getName().endsWith(".pdf"));
 
 		Arrays.asList(files)
 		.stream()
@@ -41,35 +33,8 @@ public class Main {
 		.map(BrokerReport::getExecutedOrders)
 		.flatMap(Collection::stream)
 		.filter(o -> o.getTicker() != null)
-		.sorted((a, b) -> a.getBrokerReport().getExecutionDate().compareTo(b.getBrokerReport().getExecutionDate()))//garante ordenacao
-		.map(o -> {
-			//Set asset amount by ticker
-			Integer assetAmount = assetsAmount.get(o.getTicker());
-			assetAmount = assetAmount == null ? 0 : assetAmount;
-			int assetTotal = assetAmount + o.getAmount().intValue();
-			assetsAmount.put(o.getTicker(), assetTotal);
-
-			//Group orders by ticker
-			List<Order> assetOrders = assetsOrders.get(o.getTicker());
-			if(assetOrders == null) {
-				assetOrders = new ArrayList<>();
-			}
-			assetOrders.add(o);
-
-			//Clear asset ticker if amount equals to 0
-			if(assetTotal == 0) {
-				assetOrders.clear();
-			}
-			assetsOrders.put(o.getTicker(), assetOrders);
-			return o;
-		})
-		.collect(Collectors.toList());
-
-		assetsOrders.entrySet().stream()
-		.map(e -> e.getValue())
-		.flatMap(Collection::stream)
-		.sorted((a,b) -> a.getBrokerReport().getExecutionDate().compareTo(b.getBrokerReport().getExecutionDate()))
-		.forEach(o -> {
+		.sorted((a, b) -> a.getTicker().compareTo(b.getTicker()) * 100000 + a.getBrokerReport().getExecutionDate().compareTo(b.getBrokerReport().getExecutionDate()))//garante ordenacao
+		.peek(o -> {
 			InvestmentType investmentType = o.getInvestmentType();
 			String ticker = o.getTicker();
 			BigDecimal price = o.getPrice();
@@ -79,7 +44,7 @@ public class Main {
 			BigDecimal settlement = o.getBrokerReport().getSettlementFee();
 			LocalDate executionDate = o.getBrokerReport().getExecutionDate();
 			System.out.printf(new Locale("pt", "BR"), "%s; %s; %,.2f; %,.2f; %,.2f; %,.2f; %,.2f; %s\n", investmentType, ticker, price, amount, operationPrice, liquidity, settlement, executionDate);
-			//inconsistencias: ITUB
-		});
+		})
+		.collect(Collectors.toList());
 	}
 }
